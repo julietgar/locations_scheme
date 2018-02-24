bf89a0fbca4ffe9aefe390956aa250c2f7228cd4
unknown <julietgarcia9@gmail.com>
2018-02-24 17:48:05 -0300

    Added api routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,16 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->prefix('countries')->group(function () {
+
+	Route::get('/', 'CountriesController@list');
+	Route::post('/', 'CountriesController@country');
+	Route::put('/', 'CountriesController@updateCountry');
+	Route::delete('/', 'CountriesController@deleteCountry');
+
+	Route::get('/locations', 'LocationsController@list');
+	Route::post('/locations', 'LocationsController@location');
+	Route::put('/locations', 'LocationsController@updateLocation');
+	Route::delete('/locations', 'LocationsController@deleteLocation');
+
 });
