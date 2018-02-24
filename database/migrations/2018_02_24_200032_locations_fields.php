13b34069f74ec6ffb3bb9da2ede07c9b2597d421
unknown <julietgarcia9@gmail.com>
2018-02-24 17:04:24 -0300

    Added migrations tables

---
+++ database/migrations/2018_02_24_200032_locations_fields.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class LocationsFields extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations_fields', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('countries_id')->unsigned();
+            $table->integer('sort')->unique();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+
+            $table->foreign('countries_id')
+                    ->references('id')->on('countries')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('locations_fields');
+    }
+}
