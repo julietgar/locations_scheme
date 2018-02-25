05b369e31d54915dd03b95e2cd003ec2e9eed58c
unknown <julietgarcia9@gmail.com>
2018-02-25 17:35:15 -0300

    Added Apidoc to document API

---
+++ public/apidoc/api_project.js
@@ -0,0 +1,18 @@
+define({
+  "name": "Locations Scheme",
+  "version": "1.0.0",
+  "description": "To manage a locations tree.",
+  "title": "Manage Locations Tree",
+  "template": {
+    "forceLanguage": "en"
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-02-25T20:29:53.193Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
